635532d2e91f6d6203922d1149c1602768594337
nickFalcone <nfalcone@u.rochester.edu>
2017-05-09 12:19:31 -0400

    add movement buttons

--- js/app.js
+++ js/app.js
@@ -13,6 +13,8 @@ window.addEventListener('load', function () {
   let TaxiModel = require('./taximodel');
   let TaxiView = require('./taxiview');
 
+  document.querySelector('#zero').classList.add('highlight');
+
   let taxiModel = new TaxiModel();
   taxiModel.fuel = 25;
   taxiModel.X = 0;
@@ -24,6 +26,49 @@ window.addEventListener('load', function () {
   });
 
   taxiView.render();
+
+
+  let upBtn = document.querySelector('#up');
+  upBtn.addEventListener('click', function () {
+    if (taxiModel.fuel > 0) {
+      document.querySelector('#zero').classList.remove('highlight');
+      taxiModel.fuel -= 1;
+      taxiModel.Y += 1;
+    }
+  });
+
+  let downBtn = document.querySelector('#down');
+  downBtn.addEventListener('click', function () {
+    if (taxiModel.fuel > 0) {
+      document.querySelector('#zero').classList.remove('highlight');
+      taxiModel.fuel -= 1;
+      taxiModel.Y -= 1;
+    }
+  });
+
+  let leftBtn = document.querySelector('#left');
+  leftBtn.addEventListener('click', function () {
+    if (taxiModel.fuel > 0) {
+      document.querySelector('#zero').classList.remove('highlight');
+      taxiModel.fuel -= 1;
+      taxiModel.X -= 1;
+    }
+  });
+
+  let rightBtn = document.querySelector('#right');
+  rightBtn.addEventListener('click', function () {
+    if (taxiModel.fuel > 0) {
+      document.querySelector('#zero').classList.remove('highlight');
+      taxiModel.fuel -= 1;
+      taxiModel.X += 1;
+    }
+  });
+
+
+
+
+
+
 
 
 }); // close window load
